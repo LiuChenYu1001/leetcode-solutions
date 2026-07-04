81c034f385f133daa165808a4ea2b7c72eff4e75
LiuChenYu1001 <128461273+LiuChenYu1001@users.noreply.github.com>
2026-07-05 03:42:40 +0800

    Time: 11 ms (83.21%), Space: 21.9 MB (79.48%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = defaultdict(list)
+
+        for word in strs:
+            key = "".join(sorted(word))
+            seen[key].append(word)
+
+        return list(seen.values())     
