fcd150c59a8b84643caf03f4703e8d8726cd7d59
LiuChenYu1001 <128461273+LiuChenYu1001@users.noreply.github.com>
2026-08-19 23:22:38 +0800

    Time: 115 ms (7.97%), Space: 23.5 MB (12.85%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
+        rows = {}
+        for row, seat in reservedSeats:
+            if row not in rows:
+                rows[row] = set()
+            rows[row].add(seat)
+
+        ans = (n - len(rows)) * 2
+
+        for reserved in rows.values():
+            left = all(seat not in reserved for seat in range(2, 6))
+            mid = all(seat not in reserved for seat in range(4, 8))
+            right = all(seat not in reserved for seat in range(6, 10))
+
+            if left and right:
+                ans += 2
+            elif left or mid or right:
+                ans += 1
+
+        return ans
