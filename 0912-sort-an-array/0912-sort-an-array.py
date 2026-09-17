a2c5cf8a44e5eb6a1b2852e3631db3bf2ae85b14
LiuChenYu1001 <128461273+LiuChenYu1001@users.noreply.github.com>
2026-09-17 13:48:18 +0800

    Time: 980 ms (8.39%), Space: 38.1 MB (7.15%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,29 +1,27 @@
-import random
-
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def quick_sort(left, right):
-            if left >= right:
-                return
+        n = len(nums)
 
-            pivot_idx = random.randint(left, right)
-            pivot = nums[pivot_idx]
-            lt, i, gt = left, left, right
+        def heapify(n, i):
+            largest = i
+            left = 2 * i + 1
+            right = 2 * i + 2
 
-            while i <= gt:
-                if nums[i] < pivot:
-                    nums[lt], nums[i] = nums[i], nums[lt]
-                    lt += 1
-                    i += 1
-                elif nums[i] > pivot:
-                    nums[gt], nums[i] = nums[i], nums[gt]
-                    gt -= 1
-                else:
-                    i += 1
+            if left < n and nums[left] > nums[i]:
+                largest = left
 
-            quick_sort(left, lt - 1)
-            quick_sort(gt + 1, right)
+            if right < n and nums[right] > nums[largest]:
+                largest = right
 
-        quick_sort(0, len(nums) - 1)
+            if largest != i:
+                nums[i], nums[largest] = nums[largest], nums[i]
+                heapify(n, largest)
+
+        for i in range(n // 2 - 1, -1, -1):
+            heapify(n, i)
+
+        for j in range(n - 1, 0, -1):
+            nums[j], nums[0] = nums[0], nums[j]
+            heapify(j, 0)
 
         return nums
